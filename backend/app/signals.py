989e6d0bb239033ffbf2166d7a90adce742fc0b8
AllanSilva29 <allanjulio61@gmail.com>
2025-12-20 01:40:52 -0300

    [fix] Evitando loop infinito

--- backend/app/signals.py
+++ backend/app/signals.py
@@ -5,4 +5,6 @@ from .tasks import calculate_person_stats
 
 @receiver(post_save, sender=Person)
 def trigger_stats_calculation(sender, instance, created, **kwargs):
+    if kwargs.get('update_fields') and 'stats' in kwargs['update_fields']:
+        return
     calculate_person_stats.delay(instance.id)
